c8aac141fe48a165af245d6aaf266262ab8c2b99
yutas-8 <gh01.yuta@gmail.com>
2020-09-05 21:33:49 +0900

    [Finish]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def show
+    @book=Book.find(params[:id])
+  end
+
+  def index
+    @books=Book.all
+    @book=Book.new
+  end
+
+  def new
+    @book=Book.new
+  end
+
+  def create
+    @book=Book.new(book_params)
+    @books=Book.all
+    if @book.save
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+      render action: :index
+    end
+  end
+
+  def edit
+    @book=Book.find(params[:id])
+  end
+  
+  def update
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "Book was successfully updated."
+    else
+      render action: :edit
+    end
+  end
+  
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path,notice: "Book was successfully destroyed."
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
